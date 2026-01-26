3bf56323d1f69616b874a9ba096a7417870dc038
Ryan Xu <ryan-xu@live.com>
2026-01-26 14:52:48 -0500

    Add aliases

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@ echo "Going to log directory"
 cd /var/log
 echo "Setting editor to Vim"
 export EDITOR=vim
+echo "Configuring aliases"
+alias l="ls"
+alias cls="clear"
+alias myip="curl ifconfig.me"
 
 echo "Done!"
