d688760654b57d2ab5c160ffcaaf2f5b6c5c266e
Ryan Xu <ryan-xu@live.com>
2026-01-26 14:53:43 -0500

    Formatting

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 echo "Starting setup..."
+
 echo "Going to log directory"
 cd /var/log
+
 echo "Setting editor to Vim"
 export EDITOR=vim
+
 echo "Configuring aliases"
 alias l="ls"
 alias cls="clear"
