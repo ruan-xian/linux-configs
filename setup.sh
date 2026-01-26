e4559a1060f79b8101286f6061c0364bbc031962
Ryan Xu <ryan-xu@live.com>
2026-01-26 15:22:54 -0500

    Add theme fetch

--- setup.sh
+++ setup.sh
@@ -12,5 +12,6 @@ alias cls="clear"
 alias myip="curl ifconfig.me"
 
 echo "Setting up vim"
+wget "https://raw.githubusercontent.com/ruan-xian/linux-configs/refs/heads/main/iceberg.vim" > ~/.vim/iceberg.vim
 
 echo "Done!"
