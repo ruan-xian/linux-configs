45c21f572d413c86d25dbd54ef9c68f6950908a9
Ryan Xu <ryan-xu@live.com>
2026-01-27 12:00:41 -0500

    Edit context

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ echo "Setting up functions"
     { echo "5xx Count    ,Route"; awk '/5[0-9]{2}\s+[0-9]+\s+[0-9]+ms/ {c[sprintf("%-8s%s", $(NF-4), $(NF-3))]++} END {for (k in c) print c[k] "," k}' /var/log/messages | sort -rn; } | column -t -s ","
     }
 ,get5xxContext() {
-    grep -B 10 "$1 500" /var/log/messages | less -RFX
+    grep -B 10 --color=always "$1 500" /var/log/messages | less -R
 }
 
 echo "Done!"
